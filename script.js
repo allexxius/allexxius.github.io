73c287b9112c43f683d6364f2aa022189716f082
alekss <Aleks.Nazarenko@tptlive.ee>
2024-06-07 10:44:57 +0300

    liuh

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+async function Drivers() {
+    const response = await fetch("https://api.openf1.org/v1/drivers");
+    const drivers = await response.json();
 
+    const search = prompt('Whatcha wanna search?')
+    const result = drivers.filter(checkName);
+
+    function checkName(driver) {
+        return driver = search;
+    }
+
+    checkName()
+
+    const driverContainer = document.querySelector(".data-container");
+    for (const value of drivers.slice(0, 10)) {
+        driverContainer.innerHTML += `
+        <div class="driver" >
+            <img src="${value.headshot_url}">
+            <p>${value.full_name}</p>
+            <p>${value.country_code}</p>
+            <p>${value.team_name}</p>
+        </div>`;
+    }
+
+}
+
+Drivers()
